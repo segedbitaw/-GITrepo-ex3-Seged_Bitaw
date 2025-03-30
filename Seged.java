2ce726c86844522e5afe57eb2490c2a3fe7cd6c7
seged <seged@example.com>
2025-03-31 00:16:33 +0300

    fixied code Seged.java push

--- Seged.java
+++ Seged.java
@@ -1,4 +1,4 @@
-public class Hello {
+public class Seged {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             System.out.println("My nae is Seged Bitaw..." + i);
